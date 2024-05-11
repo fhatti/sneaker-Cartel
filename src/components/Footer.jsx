b3f9065eb51c9a28189c87c2a43aecfc7d8ffe91
Vlad Purcariu <99035048+fhatti@users.noreply.github.com>
2024-05-11 23:30:50 +0200

    Added navbar and footer components Not the final design, but a step toward visualizing future website changes (#1)
    Co-authored-by: Purcariu Vlad <purcariuvlad@Purcarius-Mac-mini.local>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
+import { copyrightSign } from "../assets/icons";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="logo" width={250} height={50} />
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-paragraph sm:max-w-sm">
+            SNKR Cartel - where exclusivity meets authenticity. Explore our
+            underground haven for high-end sneakers. Not your usual plug; we're
+            the gateway to your next statement pair.
+          </p>
+          <div className="flex items-center gap-5 mt-8 mb-8">
+            {socialMedia.map((icon) => (
+              <div className="flex justify-center items-center w-12 h-12 bg-headline rounded-full hover:bg-button cursor-pointer">
+                <img src={icon.src} alt="icon-logo" width={24} height={24} />
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+            {footerLinks.map((section) => (
+              <div>
+                <h4 className="text-headline font-montserrat text-2xl leading-normal font-medium mb-6">
+                  {section.title}
+                </h4>
+                <ul>
+                  {section.links.map((link) => (
+                    <li
+                      key={link.name}
+                      className="mt-3 text-paragraph font-montserrat text-base leading-normal hover:text-button cursor-pointer"
+                    >
+                      <a href="">{link.name}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between text-headline mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="copyright sign"
+            width={20}
+            height={20}
+          />
+          <p className="text-paragraph">
+            SNKR CARTEL 2024. This territory is under our sole jurisdiction{" "}
+          </p>
+        </div>
+        <p className="text-paragraph">Terms & Condiitions</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={200} height={30} />
+        </a>
+        <h1></h1>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg  text-headline font-bold "
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+            <img src={hamburger} alt="hamburger-icon" width={25} height={25} className="-headline" />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
